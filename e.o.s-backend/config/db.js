86cf20c18f6a6305c707779fa9f819a499b0baad
The Me <kwabenaampofo5@gmail.com>
2021-05-15 07:05:58 +0000

    Switch to cloud db instance

--- e.o.s-backend/config/db.js
+++ e.o.s-backend/config/db.js
@@ -4,25 +4,15 @@ let ConnectionPool; // use a connection to ensure database connections don't die
 
 ConnectionPool = mysql.createPool({
     // a connection pool will ensure that you have at least one available database connection
-    host: 'localhost', // the pc/server on which the database is located
+    host: 'ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', // the pc/server on which the database is located
     port: 3306, // the default db port (ports are simply put points of connection btn services)
-    user: 'root', // user name for connecting to the database
-    password: '',
-    database: 'jen_commerce', // the name of the database we're connecting to
-    connectionLimit: 30 //total
-});
-
-/*
-    ConnectionPool = mysql.createPool({
-    host: 'ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    port: 3306,
-    user: 'ngckzm5ezhncjtp0',
+    user: 'ngckzm5ezhncjtp0', // user name for connecting to the database
     password: 'mzlhyrgpgb61rpc6',
-    database: 'upv2k1le2r1cwz1x',
+    database: 'upv2k1le2r1cwz1x', // the name of the database we're connecting to
     connectionLimit: 30
 });
 
-*/
+
 
 // creating a database connection
 ConnectionPool.getConnection(function database(err, connection) {
