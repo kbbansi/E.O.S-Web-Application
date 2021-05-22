6b0b1683bb3fc35a984bcf08ecdd6905ee9aa732
kbbansi <kwabenaampofo5@gmail.com>
2021-05-22 14:38:24 +0000

    Reset database

--- e.o.s-backend/config/application.sql
+++ e.o.s-backend/config/application.sql
@@ -2,21 +2,6 @@ create database if not exists jen_commerce;
 
 use jen_commerce;
 
-# Create Users SQL
-create table if not exists users
-(
-	id int auto_increment
-		primary key,
-	firstName varchar(100) default '' not null,
-	lastName varchar(100) default '' not null,
-	otherNames varchar(100) default '' null,
-	email varchar(250) default '' not null,
-	password varchar(250) default '' not null,
-	contactNo varchar(20) default '' not null,
-	userType int default 1 null
-);
-
-# Create Categories SQL
 create table if not exists categories
 (
     id           int auto_increment
@@ -25,21 +10,27 @@ create table if not exists categories
     description  varchar(300) null
 );
 
-# Create Products SQL
-create table if not exists products
+create table if not exists delivery_address
 (
-    id           int auto_increment
+    id            int auto_increment
         primary key,
-    productName  varchar(100) not null,
-    categoryID   int          not null,
-    price        double       not null,
-    productImage varchar(300) null,
-    description  varchar(300) null,
-    stock        int          not null,
-    createdOn    varchar(300) not null
+    userID        int          not null,
+    streetName    varchar(250) not null,
+    streetNo      varchar(250) not null,
+    district      varchar(250) not null,
+    region        varchar(250) not null,
+    ghanaPostCode varchar(200) null
 );
 
-# Create Order_Product
+create table if not exists order_customer
+(
+    id        int auto_increment
+        primary key,
+    orderID   int          not null,
+    userID    int          not null,
+    createdOn varchar(250) not null
+);
+
 create table if not exists order_product
 (
     id         int auto_increment
@@ -50,18 +41,21 @@ create table if not exists order_product
     createdOn  varchar(250) not null
 );
 
-
-# Create Order_Customer
-create table if not exists order_customer
+create table if not exists products
 (
-    id        int auto_increment
+    id             int auto_increment
         primary key,
-    orderID   int          not null,
-    userID    int          not null,
-    createdOn varchar(250) not null
+    productName    varchar(100) not null,
+    categoryID     int          not null,
+    price          double       not null,
+    productImage   varchar(300) null,
+    description    varchar(300) null,
+    stock          int          not null,
+    colour         varchar(200) null,
+    categoryGender int          null,
+    createdOn      varchar(300) not null
 );
 
-# Create Promotions table
 create table if not exists promotions
 (
     id            int auto_increment
@@ -73,4 +67,18 @@ create table if not exists promotions
     endDate       varchar(250)                  not null,
     status        varchar(100) default 'active' null,
     createdOn     varchar(250)                  not null
+);
+
+create table if not exists users
+(
+    id         int auto_increment
+        primary key,
+    firstName  varchar(100) default '' not null,
+    lastName   varchar(100) default '' not null,
+    otherNames varchar(100) default '' null,
+    email      varchar(250) default '' not null,
+    userName   varchar(250)            null,
+    password   varchar(250) default '' not null,
+    contactNo  varchar(20)  default '' not null,
+    userType   int          default 1  null
 );
