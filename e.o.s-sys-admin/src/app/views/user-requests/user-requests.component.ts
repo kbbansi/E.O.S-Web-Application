83e3a146c53808895b44c17b36720e8ba6f00e69
kbbansi <kwabenaampofo5@gmail.com>
2021-09-12 09:01:58 +0000

    feat(e.o.s-sys-admin): add get all user requests

--- e.o.s-sys-admin/src/app/views/user-requests/user-requests.component.ts
+++ e.o.s-sys-admin/src/app/views/user-requests/user-requests.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ApiService } from '../../../services/api.service';
 
 @Component({
   selector: 'app-user-requests',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-requests.component.scss']
 })
 export class UserRequestsComponent implements OnInit {
+  dataBucket: any = {};
+  userRequests: any = {};
+  modalRef: BsModalRef;
+  message: String;
+  email: String;
+  userName: String;
+  date: String;
+  userRequestForm: FormGroup;
 
-  constructor() { }
+  constructor(private api: ApiService, private formBuilder: FormBuilder, private modalService: BsModalService) {
+    this.createUserRequestForm();
+  }
 
   ngOnInit(): void {
+    this.getAllRequests();
   }
+
+  createUserRequestForm() {}
+
+  getAllRequests() {
+    this.api.getUserRequests().subscribe(response => {
+      console.log(response);
+      this.dataBucket = response;
+      this.userRequests = this.dataBucket.message;
+    });
+  }
+
+  openPasswordResetModal(template: TemplateRef<any>) {}
 
 }
